06b011d5ac98a0fd303d29f00ae74da97fc951eb
imcuttle <moyuyc95@gmail.com>
2019-05-07 19:58:50 +0800

    chore: for release

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default [
     input: 'index.js',
     output: [
       {
-        name: 'OpenOneWindow',
+        name: 'SnapFadeAway',
         file: pkg.browser,
         format: 'umd'
       },
